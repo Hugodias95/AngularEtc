c7e74edf1ac05310b4b3e2816aa337d04a756722
Hugodias95 <100964931+Hugodias95@users.noreply.github.com>
2023-03-15 13:10:25 -0300

    Dynamic import

---
+++ EcmaScript/import/lib.mjs
@@ -0,0 +1,6 @@
+export const saudacao = () => {
+
+    let data = new Date().toLocaleDateString()
+    return `Olá, seja muito bem-vindo. Hoje é ${data}`
+
+}
---
+++ EcmaScript/import/lib2.mjs
@@ -0,0 +1,4 @@
+export function getEnderecoByCEP(cep) {
+    return fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+}
