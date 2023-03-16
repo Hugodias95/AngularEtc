34aa846d2a1912351cd011afa8c9d10b14e63867
Hugodias95 <100964931+Hugodias95@users.noreply.github.com>
2023-03-16 09:20:55 -0300

    Inferência de tipo

---
+++ Angular/TypeScript/app2.ts
@@ -0,0 +1,7 @@
+let mensagem: String = "Seja bem vindo!!"
+let temporadasFriends: number = 10
+let estudandoAngular: boolean = true
+let listaDeFrutas: Array<String> = ['Uva','Banana','Maçã']
+let notaDasProvas: number[] = [7.5, 8, 9]
+
+//Inferência de tipo habilita a verificação estática
---
+++ Angular/TypeScript/dist/app2.js
@@ -0,0 +1,7 @@
+"use strict";
+let mensagem = "Seja bem vindo!!";
+let temporadasFriends = 10;
+let estudandoAngular = true;
+let listaDeFrutas = ['Uva', 'Banana', 'Maçã'];
+let notaDasProvas = [7.5, 8, 9];
+//Inferência de tipo habilita a verificação estática
